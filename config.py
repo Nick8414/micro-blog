c165a218da0c7e396775aa6349a135cdb3ba9891
Nikolay Rokotyansky <rokotyansky.n.1@pg.com>
2019-01-15 14:51:24 +0200

    chapter 9

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Configuration(object):
-    DEBUG = 0
+    DEBUG = 1
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
@@ -13,3 +13,4 @@ class Configuration(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['ragnos84@gmail.com']
+    POSTS_PER_PAGE = 3
